ed4a9bd0f9fedd6f62307211948ea24c35dc2445
mesoria <48843196+mesoria@users.noreply.github.com>
2019-09-15 19:31:06 -0300

    Acceso Datos

---
+++ AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class AccesoDatosManager
+    {
+        public static string cadenaConexion = "data source=LAPTOP-4ROB6KSH\\SQLEXPRESS; initial catalog=CATALOGO_DB; integrated security=sspi";
+        private SqlCommand comando;
+        private SqlConnection conexion;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public SqlCommand Comando
+        {
+            get { return comando; }
+        }
+
+        public AccesoDatosManager()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        //setear consulta embebida.
+        public void SetearConsulta(string consulta)
+        {
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        //setear consulta embebida con condicion.
+        public void SetearConsulta(string consulta, int id)
+        {
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta + id.ToString();
+        }
+
+        //esto para luego...
+        public void SetearSP(string sp)
+        {
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+        }
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int EjecutarAccionReturn()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                return (int)comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EjecutarConsulta()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ AccesoDatos/Script DB/script db.sql
@@ -0,0 +1,103 @@
+use master
+go
+create database CATALOGO_DB
+go
+use CATALOGO_DB
+go
+USE [CATALOGO_DB]
+GO
+
+/****** Object:  Table [dbo].[MARCAS]    Script Date: 08/09/2019 10:32:05 a.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[MARCAS](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](50) NULL,
+ CONSTRAINT [PK_MARCAS] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+USE [CATALOGO_DB]
+GO
+
+/****** Object:  Table [dbo].[CATEGORIAS]    Script Date: 08/09/2019 10:32:14 a.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[CATEGORIAS](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](50) NULL,
+ CONSTRAINT [PK_CATEGORIAS] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+USE [CATALOGO_DB]
+GO
+
+/****** Object:  Table [dbo].[ARTICULOS]    Script Date: 08/09/2019 10:32:24 a.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[ARTICULOS](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Codigo] [varchar](50) NULL,
+	[Nombre] [varchar](50) NULL,
+	[Descripcion] [varchar](150) NULL,
+	[IdMarca] [int] NULL,
+	[IdCategoria] [int] NULL,
+	[Imagen] [varchar](150) NULL,
+	[Precio] [money] NULL,
+ CONSTRAINT [PK_ARTICULOS] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+
+
+insert into MARCAS values ('Samsung'), ('Apple'), ('Sony'), ('Huawei'), ('Motorola')
+insert into CATEGORIAS values ('Celulares'),('Televisores'), ('Media'), ('Audio')
+insert into ARTICULOS values ('S01', 'Galaxy S10', 'Una canoa cara', 1, 1, '', 69.999),
+('M03', 'Moto G Play 7ma Gen', 'Ya siete de estos?', 1, 5, '', 15699),
+('S99', 'Play 4', 'Ya no se cuantas versiones hay', 3, 3, '', 35000),
+('S56', 'Bravia 55', 'Alta tele', 3, 2, '', 49500),
+('A23', 'Apple TV', 'lindo loro', 2, 3, '', 7850)
