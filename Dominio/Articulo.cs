6b675bbfc8a3bf4300c0a9ca033baf5163e6c28f
mesoria <48843196+mesoria@users.noreply.github.com>
2019-09-15 19:35:31 -0300

    Add files via upload

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int    Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public Marca  Marca { get; set; }
+        public CategoriaArticulo Categoria { get; set; }
+        //public float  PrecioVenta { get; set; }
+        //public float  PricioCompra { get; set; }
+        //public Stock  Stock { get; set; }
+    }
+}
---
+++ Dominio/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public int Minimo { get; set; }
+        public int Maximo { get; set; }
+        public int Actual { get; set; }
+    }
+}
