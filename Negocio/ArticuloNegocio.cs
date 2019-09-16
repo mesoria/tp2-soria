688fd097ef2da44e9c5cafcfe72af74d8f2fba9f
mesoria <48843196+mesoria@users.noreply.github.com>
2019-09-15 23:36:00 -0300

    AMD articulo

--- Negocio/ArticuloNegocio.cs
+++ Negocio/ArticuloNegocio.cs
@@ -35,7 +35,7 @@ namespace Negocio
                     aux.Categoria   = new CategoriaArticulo();
                     aux.Categoria   = categoriaNegocio.GetCategoria((Int32)accesoDatos.Lector[5]);
                     aux.Imagen      = (string)accesoDatos.Lector[6];
-                    aux.Id          = (Int32)accesoDatos.Lector[5];
+                    aux.Precio      = (float)accesoDatos.Lector[7];
            
                     lista.Add(aux);
                 }
@@ -52,5 +52,76 @@ namespace Negocio
             }
            
         }
+
+
+        public void Agregar(Articulo articulo)
+        {
+            AccesoDatosManager accesoDatos = new AccesoDatosManager();
+            try
+            {
+                accesoDatos.SetearConsulta("insert into ARTICULOS values (@Codigo, @Nombre, @Descripcion, @IdMarca, @IdCategoria, @Imagen, @Precio)");
+                accesoDatos.Comando.Parameters.Clear();
+                accesoDatos.Comando.Parameters.AddWithValue("@Codigo",      articulo.Codigo.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Nombre",      articulo.Nombre.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Descripcion", articulo.Descripcion.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@IdMarca",     articulo.Marca.Id);
+                accesoDatos.Comando.Parameters.AddWithValue("@IdCategoria", articulo.Categoria.Id);
+                accesoDatos.Comando.Parameters.AddWithValue("@Imagen", "");//articulo.Imagen.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Precio",      articulo.Precio);
+                accesoDatos.AbrirConexion();
+                accesoDatos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.CerrarConexion();
+            }
+        }
+
+        public void Modificar(Articulo articulo)
+        {
+            AccesoDatosManager accesoDatos = new AccesoDatosManager();
+            try
+            {
+                accesoDatos.SetearConsulta("update ARTICULOS Set Codigo=@Codigo, Nombre=@Nombre, Descripcion=@Descripcion, IdMarca=@IdMarca, IdCategoria=@IdCategoria, Imagen=@Imagen, Precio=@Precio Where ID=" + articulo.Id);
+                accesoDatos.Comando.Parameters.Clear();
+                accesoDatos.Comando.Parameters.AddWithValue("@Codigo",      articulo.Codigo.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Nombre",      articulo.Nombre.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Descripcion", articulo.Descripcion.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@IdMarca",     articulo.Marca.Id);
+                accesoDatos.Comando.Parameters.AddWithValue("@IdCategoria", articulo.Categoria.Id);
+                accesoDatos.Comando.Parameters.AddWithValue("@Imagen", "");//articulo.Imagen.ToString());
+                accesoDatos.Comando.Parameters.AddWithValue("@Precio",      articulo.Precio);
+                accesoDatos.AbrirConexion();
+                accesoDatos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.CerrarConexion();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            AccesoDatosManager datos = new AccesoDatosManager();
+            try
+            {
+                datos.SetearConsulta("delete from ARTICULOS where Id =" + id);
+                datos.AbrirConexion();
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
